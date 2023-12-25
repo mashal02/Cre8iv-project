0b4bbd77277c2bb5422b4b7899196a8c8bc54509
shifa309 <shifaimran309@gmail.com>
2023-12-25 09:41:31 +0500

    2nd post request in process

--- frontend/src/components/messagesent.js
+++ frontend/src/components/messagesent.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 const ImageUpload = () => {
   const [message, setMessage] = useState('');
   const [receivedMessage, setReceivedMessage] = useState('');
+  const [secondMessage, setSecondMessage] = useState('');
+  const [receivedSecondMessage, setReceivedSecondMessage] = useState('');
 
   const handleSendMessage = async () => {
     try {
@@ -25,22 +27,58 @@ const ImageUpload = () => {
     }
   };
 
+  const handleSendSecondMessage = async () => {
+    try {
+      const response = await axios.post(
+        'https://localhost:3001/send-category',
+        { message: secondMessage },
+        {
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      // Update the received second message in the state
+      setReceivedSecondMessage(response.data.message);
+    } catch (error) {
+      console.error('Error sending second message:', error);
+    }
+  };
+
   return (
     <div>
-      c
       <div>
-        <label>Enter Message:</label>
+        <label>Enter First Message:</label>
         <input
           type="text"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
       </div>
-      <button onClick={handleSendMessage}>Send Message</button>
+      <button onClick={handleSendMessage}>Send First Message</button>
       {receivedMessage && (
         <div>
           <p>Received Message from Server:</p>
           <p>{receivedMessage}</p>
+        </div>
+      )}
+
+      {/* Add input and button for the second message */}
+      <div>
+        <label>Enter Second Message:</label>
+        <input
+          type="text"
+          value={secondMessage}
+          onChange={(e) => setSecondMessage(e.target.value)}
+        />
+      </div>
+      <button onClick={handleSendSecondMessage}>Send Second Message</button>
+      {receivedSecondMessage && (
+        <div>
+          <p>Received Second Message from Server:</p>
+          <p>{receivedSecondMessage}</p>
         </div>
       )}
     </div>
