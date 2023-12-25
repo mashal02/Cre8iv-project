0ef0455c6ad681a2f99b56ad01e6ef8455cb6f5e
shifa309 <shifaimran309@gmail.com>
2023-12-25 23:05:58 +0500

    ImageUpload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 //import Features from './components/features';
 //import Login from './components/login';
 //import Signup from './components/signup';
-//import UploadImg from './components/uploadImg';
-import ImageUpload from './components/messagesent';
+import UploadImg from './components/uploadImg';
+//import ImageUpload from './components/messagesent';
 
 function App() {
   return (
     <>
-      <ImageUpload />
+      <UploadImg />
     </>
   );
 }
