2241c0cf886ecd0cf4fb73368f763140206bbe06
Mashal Ashfaque <mashfaque.bscs20seecs@seecs.edu.pk>
2023-12-25 10:30:17 +0500

    Login testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 //import Login from './components/login';
 //import Signup from './components/signup';
 //import UploadImg from './components/uploadImg';
-import ImageUpload from './components/messagesent';
+//import ImageUpload from './components/messagesent';
+import Login from './components/login2';
 
 function App() {
   return (
     <>
-      <ImageUpload />
+      <Login />
     </>
   );
 }
