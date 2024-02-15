bce8f7f2853c48889ea9fa0d445afaf88080b169
Sadiya Haleema <sadiyyahaleema282001@gmail.com>
2024-02-15 10:39:06 +0500

    Fb login for Business Changes Made

--- src/api/wrapper.js
+++ src/api/wrapper.js
@@ -27,8 +27,8 @@ export const apiWrapper = async ({
   try {
     //const url = `https://192.168.1.28:3001/${endpoint}`;
     //const url = `https://192.168.56.1:3001/${endpoint}`;
-    const url = `https://localhost:3001/${endpoint}`;
-    //const url = `https://10.7.116.80:3001/${endpoint}`
+    //const url = `https://localhost:3001/${endpoint}`;
+    const url = `https://10.7.117.95:3001/${endpoint}`
 
     console.log(url);
     const response = await axios({
