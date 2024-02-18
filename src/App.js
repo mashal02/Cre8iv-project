f275af202e59e681032d5e3b2cf2bf2ab6fc0ba8
Shifa Imran <shifaimran309@gmail.com>
2024-02-18 23:30:31 +0500

    faq page

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import './css/App.css';
 import Home from './pages/home';
 //import FacebookLoginComponent from './pages/login';
 import Login from './pages/login';
-
 import Features from './pages/features';
 //import Signup from './components/signup';
 //import UploadImg from './components/uploadImg';
 //import ImageUpload from './components/messagesent';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { FAQ } from './pages/faq';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" exact element={<Home />} />
-        <Route path="/login" exact element={<Login/>} />
+        <Route path="/login" exact element={<Login />} />
         <Route path="/features" exact element={<Features />} />
-
+        <Route path="/faq" exact element={<FAQ />} />
       </Routes>
     </Router>
   );
